7ec91eed2a56d753c3da67f18a2f7842d833d5f9
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-03 16:51:22 +0900

    add comments

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const port = 3000;
 app.use(cors());
 app.use(session({
   secret: process.env.SESSION_SECRET || 'default_secret_key',//セッションIDの署名に使う秘密鍵
-  resave: false,
+  resave: false,//変更がない限り保存しない
   saveUninitialized: true,//未初期化セッションの保存
   cookie: {
-    secure: false, // HTTPSでない場合はfalse
-    maxAge: 1000 * 60 * 60 * 24, //ミリセコンド 1D
+    httpOnly: true,//JSからアクセス不可
+    secure: false, //httpsか
+    maxAge: 1000 * 60 * 30, //ミリセコンド表記 30Min
   },
 }));
 
