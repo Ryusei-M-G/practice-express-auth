90c8d35f742b929c665b5c3d7b7523de5654bc04
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-04 16:56:21 +0900

    Add jwtAuth.js

---
+++ controllers/jwtAuth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { addUser, getUserByUsername } from '../dbcon.js';
+
+// JWT方式のログイン処理
+export const jwtLogin = async (req, res) => {
+  
+};
+
+// JWT方式の登録処理
+export const jwtRegister = async (req, res) => {
+};
+
+// JWT方式のログアウト処理
+export const jwtLogout = (req, res) => {
+  
+};
+
+// JWT方式のユーザー情報取得
+export const getJwtUserInfo = (req, res) => {
+  
+};
