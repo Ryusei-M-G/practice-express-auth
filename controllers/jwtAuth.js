ad97f1c671b3ea2180455a2eaa41fce92eb63445
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-04 17:18:48 +0900

    add jwtRegister\ \ add jwt register

--- controllers/jwtAuth.js
+++ controllers/jwtAuth.js
@@ -4,19 +4,51 @@ import { addUser, getUserByUsername } from '../dbcon.js';
 
 // JWT方式のログイン処理
 export const jwtLogin = async (req, res) => {
-  
+
 };
 
 // JWT方式の登録処理
 export const jwtRegister = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: 'username and password are required' });
+  }
+
+  try {
+    const newUser = await addUser(username, password);
+
+    //jwt生成
+    const token = jwt.sign(
+      {
+        userId: newUser.id,
+        username: username
+      },
+      process.env.JWT_SECRET || 'fallback_secret',{
+        expiresIn: '1h'
+      }
+    );
+
+    //token返却
+    res.status(201).json({
+      message:'registered successfully',
+      token: token,
+      user: {username: username}
+    });
+  }catch(error){
+    if(error.code === '23505'){
+      return res.status(409).json({message: 'Username already exists'});
+    }
+    console.error(error);
+    res.status(500).json({message:'Internal server error'});
+  }
 };
 
 // JWT方式のログアウト処理
 export const jwtLogout = (req, res) => {
-  
+
 };
 
 // JWT方式のユーザー情報取得
 export const getJwtUserInfo = (req, res) => {
-  
+
 };
